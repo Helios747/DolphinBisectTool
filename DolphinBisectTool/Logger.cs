dfaf5b887efd8b808f2022e16d445ff993dc02cc
Anthony <aserna3@gmail.com>
2017-08-30 14:32:22 -0700

    [Logger] Dumb simple logging implementation

---
+++ DolphinBisectTool/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DolphinBisectTool
+{
+    class Logger
+    {
+        StreamWriter log_file;
+
+        public Logger()
+        {
+            log_file = new StreamWriter("log-" + DateTime.Now.ToString("yyyy-MM-dd_hhmmss") + ".txt");
+        }
+
+        ~Logger()
+        {
+            log_file.Close();
+        }
+
+        public void Write(string s)
+        {
+            log_file.WriteLine(s);
+            log_file.Flush();
+        }
+
+    }
+}
